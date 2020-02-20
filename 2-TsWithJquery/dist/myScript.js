061695288950ce71aa39a3f57b01c7586e47b93c
LeelK <karunarathne1995@gmail.com>
2020-02-20 12:20:29 +0530

    js function

--- 2-TsWithJquery/dist/myScript.js
+++ 2-TsWithJquery/dist/myScript.js
@@ -6,3 +6,10 @@ $("h1").css('color', 'red');
 $("#btnClickMe").click(function () {
     alert("Working");
 });
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane User";
+$("#box").click(function () {
+    alert(greeter(user));
+});
--- 2-TsWithJquery/src/myScript.ts
+++ 2-TsWithJquery/src/myScript.ts
@@ -7,3 +7,14 @@ $("h1").css('color','red');
 $("#btnClickMe").click(function(){
     alert("Working");
 });
+
+
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+$("#box").click(function () {
+    alert(greeter(user));
+});
